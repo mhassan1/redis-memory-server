3cf0118bd39d6608aa1232ee7185abce802ce9bd
Shimon Doodkin <helpmepro1@gmail.com>
2021-09-18 01:59:51 +0300

    feat(windows): download url for windows, update ts types package for process.plaform

--- src/util/RedisBinaryDownloadUrl.ts
+++ src/util/RedisBinaryDownloadUrl.ts
@@ -22,14 +22,18 @@ export default class RedisBinaryDownloadUrl {
    * Calls all the necessary functions to determine the URL
    */
   async getDownloadUrl(): Promise<string> {
-    const archive = await this.getArchiveName();
-    log(`Using "${archive}" as the Archive String`);
-
     const downloadUrl = resolveConfig('DOWNLOAD_URL');
     if (downloadUrl) {
       log(`Using "${downloadUrl}" as the Download-URL`);
       return downloadUrl;
     }
+
+    if (process.platform === 'win32') {
+      return 'https://github.com/ServiceStack/redis-windows/raw/master/downloads/redis-latest.zip';
+    }
+
+    const archive = await this.getArchiveName();
+    log(`Using "${archive}" as the Archive String`);
 
     const mirror = resolveConfig('DOWNLOAD_MIRROR') ?? 'https://download.redis.io';
     log(`Using "${mirror}" as the mirror`);
@@ -42,6 +46,9 @@ export default class RedisBinaryDownloadUrl {
    * Version independent
    */
   async getArchiveName(): Promise<string> {
+    if (process.platform === 'win32') {
+      return 'redis-latest.zip';
+    }
     return `redis-${this.version}.tar.gz`;
   }
 }
--- src/util/__tests__/RedisBinaryDownloadUrl-test.ts
+++ src/util/__tests__/RedisBinaryDownloadUrl-test.ts
@@ -3,6 +3,20 @@ import RedisBinaryDownloadUrl from '../RedisBinaryDownloadUrl';
 describe('RedisBinaryDownloadUrl', () => {
   describe('getDownloadUrl()', () => {
     describe('for mac', () => {
+      let originalPlatform: string;
+      beforeAll(() => {
+        originalPlatform = process.platform;
+        Object.defineProperty(process, 'platform', {
+          value: 'darwin',
+        });
+      });
+
+      afterAll(() => {
+        Object.defineProperty(process, 'platform', {
+          value: originalPlatform,
+        });
+      });
+
       it('stable', async () => {
         const du = new RedisBinaryDownloadUrl({
           version: 'stable',
@@ -18,6 +32,40 @@ describe('RedisBinaryDownloadUrl', () => {
         });
         expect(await du.getDownloadUrl()).toBe(
           'https://download.redis.io/releases/redis-6.0.10.tar.gz'
+        );
+      });
+    });
+
+    describe('for win32', () => {
+      let originalPlatform: string;
+      beforeAll(() => {
+        originalPlatform = process.platform;
+        Object.defineProperty(process, 'platform', {
+          value: 'win32',
+        });
+      });
+
+      afterAll(() => {
+        Object.defineProperty(process, 'platform', {
+          value: originalPlatform,
+        });
+      });
+
+      it('stable', async () => {
+        const du = new RedisBinaryDownloadUrl({
+          version: 'stable',
+        });
+        expect(await du.getDownloadUrl()).toBe(
+          'https://github.com/ServiceStack/redis-windows/raw/master/downloads/redis-latest.zip' // its version 3.0.503, no other version available, hopefully good enough for testing
+        );
+      });
+
+      it('6.0.10', async () => {
+        const du = new RedisBinaryDownloadUrl({
+          version: '6.0.10',
+        });
+        expect(await du.getDownloadUrl()).toBe(
+          'https://github.com/ServiceStack/redis-windows/raw/master/downloads/redis-latest.zip' // its version 3.0.503, no other version available, hopefully good enough for testing
         );
       });
     });

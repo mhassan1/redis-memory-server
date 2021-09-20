4d55ad39466cbc78908f9daa948140f3f5a38032
Shimon Doodkin <helpmepro1@gmail.com>
2021-09-20 09:48:40 +0300

    Revert "feat(windows): build automatically when npm installing from github"
    This reverts commit 3a7621cc3ba81d8797d4954a1e3d52619f8bae34.

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -10,20 +10,6 @@ It helps to skip timeout setup `jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;`
 when first test run hits Redis binary downloading to the cache.
 */
 
-const { spawn } = require('child_process');
-const exec = (commands) => {
-  return new Promise((resolve, reject) => {
-    const tool = spawn(commands, { stdio: 'inherit', shell: true });
-    tool.on('exit', (code) => {
-      if (code !== 0) {
-        reject();
-      } else {
-        resolve();
-      }
-    });
-  });
-};
-
 function isModuleExists(name) {
   try {
     return !!require.resolve(name);
@@ -32,48 +18,41 @@ function isModuleExists(name) {
   }
 }
 
-(async () => {
-  if (!isModuleExists('../lib/util/resolve-config')) {
-    console.log('Could not find built ts files, running tsc');
-    await exec('npm run build');
-  }
+if (!isModuleExists('../lib/util/resolve-config')) {
+  console.log('Could not resolve postinstall configuration');
+  return;
+}
 
-  if (!isModuleExists('../lib/util/resolve-config')) {
-    console.log('Could not resolve postinstall configuration');
-    return;
-  }
+const rc = require('../lib/util/resolve-config');
+rc.findPackageJson(process.env.INIT_CWD);
 
-  const rc = require('../lib/util/resolve-config');
-  rc.findPackageJson(process.env.INIT_CWD);
+const envDisablePostinstall = rc.default('DISABLE_POSTINSTALL');
 
-  const envDisablePostinstall = rc.default('DISABLE_POSTINSTALL');
+if (typeof envDisablePostinstall === 'string' && rc.envToBool(envDisablePostinstall)) {
+  console.log('Download is skipped by REDISMS_DISABLE_POSTINSTALL variable');
+  process.exit(0);
+}
 
-  if (typeof envDisablePostinstall === 'string' && rc.envToBool(envDisablePostinstall)) {
-    console.log('Download is skipped by REDISMS_DISABLE_POSTINSTALL variable');
-    process.exit(0);
-  }
+const envSystemBinary = rc.default('SYSTEM_BINARY');
 
-  const envSystemBinary = rc.default('SYSTEM_BINARY');
+if (typeof envSystemBinary === 'string') {
+  console.log('Download is skipped by REDISMS_SYSTEM_BINARY variable');
+  process.exit(0);
+}
 
-  if (typeof envSystemBinary === 'string') {
-    console.log('Download is skipped by REDISMS_SYSTEM_BINARY variable');
-    process.exit(0);
-  }
+const redisBinaryModule = '../lib/util/RedisBinary';
+if (isModuleExists(redisBinaryModule)) {
+  const RedisBinary = require(redisBinaryModule).default;
 
-  const redisBinaryModule = '../lib/util/RedisBinary';
-  if (isModuleExists(redisBinaryModule)) {
-    const RedisBinary = require(redisBinaryModule).default;
-
-    console.log('redis-memory-server: checking Redis binaries cache...');
-    RedisBinary.getPath({})
-      .then((binPath) => {
-        console.log(`redis-memory-server: binary path is ${binPath}`);
-      })
-      .catch((err) => {
-        console.log(`failed to download/install Redis binaries. The error: ${err}`);
-        process.exit(1);
-      });
-  } else {
-    console.log("Can't resolve RedisBinary module");
-  }
-})();
+  console.log('redis-memory-server: checking Redis binaries cache...');
+  RedisBinary.getPath({})
+    .then((binPath) => {
+      console.log(`redis-memory-server: binary path is ${binPath}`);
+    })
+    .catch((err) => {
+      console.log(`failed to download/install Redis binaries. The error: ${err}`);
+      process.exit(1);
+    });
+} else {
+  console.log("Can't resolve RedisBinary module");
+}
